398eb165a6e8849b794d0fc64a82c05bd6646c85
sanggonlee <lee.sg16@gmail.com>
2016-04-10 18:30:07 +0000

    commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var routes = require('./app/routes/index.js');
 //var mongoose = require('mongoose');
 var session = require('express-session');
+var mongoClient = require('mongodb').MongoClient;
 
 var app = express();
 require('dotenv').load();
@@ -22,6 +23,25 @@ app.use(session({
 }));
 
 routes(app);
+
+app.get('/new/:url', function(req, res) {
+	var inputUrl = req.params.url;
+	var obj = {
+		"original_url": inputUrl
+	};
+	console.log("in: "+JSON.stringify(obj));
+	
+	mongoClient.connect(process.env.MONGO_URI || mongoDevUri, function(err, db) {
+		db.collection('urls').insert(obj, function(err, data) {
+			if(err) {
+				throw err;
+			}
+			console.log(JSON.stringify(data));
+			obj.short_url = process.env.APP_URL + data.insertedCount;
+		});
+	});
+	return res.end(JSON.stringify(obj));
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
